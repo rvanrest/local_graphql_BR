1503bcdeb9ab27488e6507203aa25f92e056b0e5
Remo van Rest <rrest@zinl.nl>
2026-07-01 09:51:38 +0200

    macos shell-script

--- start.bat
+++ start.bat
@@ -1,4 +1,8 @@
 @echo off
+REM Dit script gaat uit van een portable node installatie
+REM In het voorbeeld hieronder is deze geinstalleerd in:
+REM C:\Users\RVR\Documents\GraphQL\node;
+
 SET PATH=C:\Users\RVR\Documents\GraphQL\node;%PATH%
 cd /d %~dp0
 node server_bemiddeling.js
